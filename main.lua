00e9292a32d45d9ea5a543eb077870ab33e4bbe6
Sharon Prabhu <sharongraceprabhu@gmail.com>
2024-07-11 12:57:09 +0530

    Added print messages and instructions for the user

--- main.lua
+++ main.lua
@@ -2,5 +2,16 @@
 
 local hackathon = require("hackathon")
 
+print("Welcome to the Hackathon Challenge!\n") 
+print("Our STEM girly is ready to participate in her first hackathon, but she needs your help to complete her project successfully.")
+print("In this challenge, you will be given different stages of the hackathon process.\n")
+print("Your task is to arrange these stages in the correct order to ensure the project is completed efficiently.") 
+print("For each prompt, you will see a list of steps numbered 1 to 4.")
+print("Enter the order of the steps by their numbers, separated by spaces (e.g. 1 2 3 4).\n")
+print("Are you ready to start? Let's go!")
+
 hackathon.run()
+
+print("\n")
+print("Thank you for playing Hackathon!")
 
