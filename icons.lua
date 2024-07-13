7f81fc7b8486d1ed9c0de8449f9fd7a164e34323
Ananya Raman <ananyar@AnanyaR>
2024-07-13 15:50:40 +0530

    Added Light Toggle Function

--- icons.lua
+++ icons.lua
@@ -5,14 +5,21 @@ local iconPositions = {}
 local iconSelected = 1
 local iconScale = 0.35
 
-local buttonLeft = {
+local backButton = {
     x = 100,
     y = 900,
     width = 50,
     height = 50
 }
 
-local buttonRight = {
+local OKButton = {
+    x = 200,
+    y = 900,
+    width = 50,
+    height = 50
+}
+
+local forwardButton = {
     x = 300,
     y = 900,
     width = 50,
@@ -48,10 +55,13 @@ function icon.draw()
     end
 
     love.graphics.setColor(1, 1, 1)
-    love.graphics.rectangle("fill", buttonLeft.x, buttonLeft.y, buttonLeft.width, buttonLeft.height)
+    love.graphics.rectangle("fill", backButton.x, backButton.y, backButton.width, backButton.height)
 
     love.graphics.setColor(1, 1, 1)
-    love.graphics.rectangle("fill", buttonRight.x, buttonRight.y, buttonRight.width, buttonRight.height)
+    love.graphics.rectangle("fill", OKButton.x, OKButton.y, OKButton.width, OKButton.height)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle("fill", forwardButton.x, forwardButton.y, forwardButton.width, forwardButton.height)
 end
 
 function icon.selectNext()
@@ -68,16 +78,37 @@ function icon.selectPrevious()
     end
 end
 
+function icon.executeAction()
+    if iconSelected == 1 then
+        print("Clean action executed")
+        -- Add the action for the clean icon
+    elseif iconSelected == 2 then
+        print("Food action executed")
+        -- Add the action for the food icon
+    elseif iconSelected == 3 then
+        print("Game action executed")
+        -- Add the action for the game icon
+    elseif iconSelected == 4 then
+        print("Medicine action executed")
+        -- Add the action for the medicine icon
+    end
+end
+
 function icon.mousepressed(x, y, button)
     if button == 1 then
-        if x >= buttonLeft.x and x <= buttonLeft.x + buttonLeft.width and
-           y >= buttonLeft.y and y <= buttonLeft.y + buttonLeft.height then
+        if x >= backButton.x and x <= backButton.x + backButton.width and
+           y >= backButton.y and y <= backButton.y + backButton.height then
             icon.selectPrevious()
         end
 
-        if x >= buttonRight.x and x <= buttonRight.x + buttonRight.width and
-           y >= buttonRight.y and y <= buttonRight.y + buttonRight.height then
+        if x >= forwardButton.x and x <= forwardButton.x + forwardButton.width and
+           y >= forwardButton.y and y <= forwardButton.y + forwardButton.height then
             icon.selectNext()
+        end
+
+        if x >= OKButton.x and x <= OKButton.x + OKButton.width and
+           y >= OKButton.y and y <= OKButton.y + OKButton.height then
+            icon.executeAction()
         end
     end
 end
