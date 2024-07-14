20e71f290225fe3a06deb848be658c10b568ed12
Shreya Agarwal <stargazing1018@gmail.com>
2024-07-14 18:21:17 +0530

    Added main.lua for the duck&girl game

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 local animation = require("animation")
 local icon = require("icons")
+local duckgame = require("duckgame")
 
 local background 
+local currentGame
 
 function love.load()
     love.window.setMode(1600, 1033)
@@ -9,27 +11,50 @@ function love.load()
 
     animation.load()
     icon.load()
+    duckgame.load()
 
     background = love.graphics.newImage("assets/background.png")
+
+    -- Set the initial game
+    currentGame = "main"
 end
 
 function love.update(dt)
-    animation.update(dt)
+    if currentGame == "main" then
+        animation.update(dt)
+    elseif currentGame == "duckgame" then
+        duckgame.update(dt)
+    end
 end
 
 function love.draw()
-    love.graphics.draw(background, 0, 0)
+    if currentGame == "main" then
+        love.graphics.draw(background, 0, 0)
 
-    if not icon.isLightOn() then
-        love.graphics.setColor(0, 0, 0)
-        love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-        love.graphics.setColor(1, 1, 1)  
+        if not icon.isLightOn() then
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+            love.graphics.setColor(1, 1, 1)  
+        end
+
+        animation.draw()
+        icon.draw()
+    elseif currentGame == "duckgame" then
+        duckgame.draw()
     end
-
-    animation.draw()
-    icon.draw()
 end
 
 function love.mousepressed(x, y, button)
-    icon.mousepressed(x, y, button)
+    if currentGame == "main" then
+        icon.mousepressed(x, y, button)
+    end
 end
+
+function love.keypressed(key)
+    if key == "1" then
+        currentGame = "main"
+    elseif key == "2" then
+        currentGame = "duckgame"
+    end
+end
+
