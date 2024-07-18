d8349244944063457de5fa8773e4d04593e8590c
Ananya Raman <ananyar@AnanyaR>
2024-07-18 11:04:00 +0530

    Updated icons for health meter function

--- icons.lua
+++ icons.lua
@@ -14,6 +14,8 @@ local lightIconScale = 0.3
 local lightOn = true  
 local sleepImage
 
+local eatIconClicked = false
+
 local backButton = {
     x = 100,
     y = 900,
@@ -113,6 +115,7 @@ function icon.executeAction()
     if iconSelected == 1 then
         -- clean func
     elseif iconSelected == 2 then
+        eatIconClicked = true  
         eat.start()
     elseif iconSelected == 3 then
         -- game func
@@ -153,6 +156,14 @@ function icon.mousepressed(x, y, button)
             icon.executeAction()
         end
     end
+end
+
+function icon.resetEatIconClicked()
+    eatIconClicked = false
+end
+
+function icon.isEatIconClicked()
+    return eatIconClicked
 end
 
 return icon
