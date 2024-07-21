2a89e40650d6cca3cca860749f2dca14fb5a8499
Ananya Raman <ananyar@AnanyaR>
2024-07-21 15:52:56 +0530

    Updated main to include intro page

--- main.lua
+++ main.lua
@@ -1,20 +1,23 @@
+local intro = require("intro")
 local icon = require("icons")
 local animation = require("animation")
-local icon = require("icons")
 local health_meter = require("health_meter")
 local eat = require("eat")
 local med = require("med")
 local game = require("game")
 
-local background 
+local background
+local pixelFont
 
 function love.load()
     love.window.setMode(1600, 1033)
     love.window.setTitle("WEP✨")
 
+    pixelFont = love.graphics.newFont("assets/fonts/pixelfont.otf", 20)
+
+    intro.load(pixelFont)
     icon.load()
     animation.load()
-    icon.load()
     health_meter.load()
     eat.load()
     med.load()
@@ -24,31 +27,45 @@ function love.load()
 end
 
 function love.update(dt)
-    
-    animation.update(dt)
-    health_meter.update(dt)
-    eat.update(dt)
-    med.update(dt)
-    game.update(dt)
+    if intro.isActive() then
+        intro.update(dt)
+    else
+        animation.update(dt)
+        health_meter.update(dt)
+        eat.update(dt)
+        med.update(dt)
+        game.update(dt)
+    end
 end
 
 function love.draw()
-    love.graphics.draw(background, 0, 0)
+    if intro.isActive() then
+        love.graphics.clear(1, 1, 0.8)
+        intro.draw()
+    else
+        love.graphics.draw(background, 0, 0)
 
-    if not icon.isLightOn() then
-        love.graphics.setColor(0, 0, 0)
-        love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-        love.graphics.setColor(1, 1, 1)
+        if not icon.isLightOn() then
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+            love.graphics.setColor(1, 1, 1)
+        end
+
+        icon.draw()
+        animation.draw()
+        health_meter.draw()
+        eat.draw()
+        med.draw()
+        game.draw()
+
+        love.graphics.setFont(pixelFont)
     end
-
-    icon.draw()
-    animation.draw()
-    health_meter.draw()
-    eat.draw()
-    med.draw()
-    game.draw()
 end
 
 function love.mousepressed(x, y, button)
-    icon.mousepressed(x, y, button)
+    if intro.isActive() then
+        intro.mousepressed(x, y, button)
+    else
+        icon.mousepressed(x, y, button)
+    end
 end
