5c306595acc8ab4a426c076a2de0097372203f7d
Sharon Prabhu <sharongraceprabhu@gmail.com>
2024-07-22 23:04:43 +0530

    Deleted health meter which has been replaced with 2 separate hunger and energy meters. Deleted scrapped mini game

--- intro.lua
+++ intro.lua
@@ -2,12 +2,12 @@
 local intro = {}
 
 local font
-local message = [[our little WE gurly is preparing for her long journey ahead as a woman in STEM!
-while she's busy brushing up on her collatz sequence, she might forget to eat or take a shower (it's completely normal, we promise)
+local message = [[Our little WE gurly is preparing for her long journey ahead as a woman in STEM!
+While she's busy brushing up on her collatz sequence, she might forget to eat or take a shower (it's completely normal, we promise)
 soooo, it is up to you, yes, you to look after her
 make sure our future leader is well fed, clean, takes her medicine, relaxed and MOST IMPORTANTLY - WELL RESTED!
 
-we trust you, okay tc! ]]
+We trust you, okay tc! ]]
 local okayButton = {x = 750, y = 800, width = 100, height = 50}
 local showIntro = true
 
