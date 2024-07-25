38b7e69e189ecb0f3ccc3083fb550162fc978b26
Shreya Agarwal <stargazing1018@gmail.com>
2024-07-25 11:48:35 +0530

    fixed scaling issue in animation.lua

--- animation.lua
+++ animation.lua
@@ -10,6 +10,8 @@ local animationSpeed = 1
 local animationIndex = 1
 local sleep
 
+local windowWidth, windowHeight
+
 function anim.load()
     girlSprites = {
         love.graphics.newImage("assets/girl/neutL.png"),
@@ -17,6 +19,7 @@ function anim.load()
     }
     currentSprite = girlSprites[1]
     sleep = love.graphics.newImage("assets/girl/sleep.png")
+    windowWidth, windowHeight = love.graphics.getDimensions()
 end
 
 function anim.update(dt)
@@ -31,11 +34,15 @@ function anim.update(dt)
 end
 
 function anim.draw()
+    love.graphics.push()
+    love.graphics.scale(windowWidth / 1920, windowHeight / 1080) -- Assuming original design resolution is 1920x1080
     if icon.isLightOn() then
         love.graphics.draw(currentSprite, 550, 425)
     else
         love.graphics.draw(sleep, 550, 500)
     end
+    love.graphics.pop()
 end
 
 return anim
+
