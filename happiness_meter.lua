8b1d18458c05228ca46758c6aad5243d62845177
Shreya Agarwal <stargazing1018@gmail.com>
2024-07-25 11:53:09 +0530

    fixed scaling issue in happiness_meter.lua

--- happiness_meter.lua
+++ happiness_meter.lua
@@ -1,50 +1,56 @@
-local happy = {}
+local happiness = {}
 
 local icon = require("icons")
-local game = require("game")
 
-local happy_meter = {}
-local happy_meter_percent = 100
-local current_happy_meter_sprite
+local happiness_meter = {}
+local happiness_meter_percent = 100
+local current_happiness_meter_sprite
 
-function happy.load()
+local windowWidth, windowHeight
+
+function happiness.load()
     for i = 1, 5 do
-        happy_meter[i] = love.graphics.newImage("assets/meters/happiness_meter (" .. i .. ").png")
+        happiness_meter[i] = love.graphics.newImage("assets/meters/happiness_meter (" .. i .. ").png")
     end
-    current_happy_meter_sprite = happy_meter[1]
+    current_happiness_meter_sprite = happiness_meter[1]
+    windowWidth, windowHeight = love.graphics.getDimensions()
 end
 
-function happy.update(dt)
-    if icon.isGameIconClicked() then
-            increaseHappy(100)
-        icon.resetGameIconClicked()
+function happiness.update(dt)
+    if icon.isFunIconClicked() then
+        increaseHappiness(20)
+        icon.resetFunIconClicked()
     end
 
-    local decrease_rate = 2
-    happy_meter_percent = happy_meter_percent - dt * decrease_rate
-    if happy_meter_percent < 0 then
-        happy_meter_percent = 0
+    local decrease_rate = 1.5
+    happiness_meter_percent = happiness_meter_percent - dt * decrease_rate
+    if happiness_meter_percent < 0 then
+        happiness_meter_percent = 0
     end
 
-    local meter_index = math.ceil((1 - happy_meter_percent / 100) * #happy_meter)
+    local meter_index = math.ceil((happiness_meter_percent / 100) * #happiness_meter)
     if meter_index < 1 then
         meter_index = 1
-    elseif meter_index > #happy_meter then
-        meter_index = #happy_meter
+    elseif meter_index > #happiness_meter then
+        meter_index = #happiness_meter
     end
 
-    current_happy_meter_sprite = happy_meter[meter_index]
+    current_happiness_meter_sprite = happiness_meter[meter_index]
 end
 
-function increaseHappy(amount)
-    happy_meter_percent = happy_meter_percent + amount
-    if happy_meter_percent > 100 then
-        happy_meter_percent = 100
+function increaseHappiness(amount)
+    happiness_meter_percent = happiness_meter_percent + amount
+    if happiness_meter_percent > 100 then
+        happiness_meter_percent = 100
     end
 end
 
-function happy.draw()
-    love.graphics.draw(current_happy_meter_sprite, 1310, 10, 0, 0.85)
+function happiness.draw()
+    love.graphics.push()
+    love.graphics.scale(windowWidth / 1920, windowHeight / 1080) -- Assuming original design resolution is 1920x1080
+    love.graphics.draw(current_happiness_meter_sprite, 1212, 85, 0, 0.85)
+    love.graphics.pop()
 end
 
-return happy
+return happiness
+
